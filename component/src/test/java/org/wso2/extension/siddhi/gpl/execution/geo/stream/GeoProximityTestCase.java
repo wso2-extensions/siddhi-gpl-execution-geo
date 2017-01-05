cdc93443514353957b7f7d42bcccbfa31ddda83d
Sajith Dimal <sajithd@wso2.com>
2017-01-05 16:07:54 +0530

    Remove release.property file. (#3)
    * Remove release.property file.
    
    * Fixing Geo Proximity Test case by handling events map assertion.

--- component/src/test/java/org/wso2/extension/siddhi/gpl/execution/geo/stream/GeoProximityTestCase.java
+++ component/src/test/java/org/wso2/extension/siddhi/gpl/execution/geo/stream/GeoProximityTestCase.java
@@ -21,43 +21,46 @@ package org.wso2.extension.siddhi.gpl.execution.geo.stream;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
+import org.wso2.extension.siddhi.gpl.execution.geo.GeoTestCase;
 import org.wso2.siddhi.core.ExecutionPlanRuntime;
 import org.wso2.siddhi.core.event.Event;
 import org.wso2.siddhi.core.query.output.callback.QueryCallback;
 import org.wso2.siddhi.core.util.EventPrinter;
-import org.wso2.extension.siddhi.gpl.execution.geo.GeoTestCase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GeoProximityTestCase extends GeoTestCase {
     private static Logger logger = Logger.getLogger(GeoProximityTestCase.class);
-    ArrayList<String> expectedResultId;
+    private ArrayList<HashMap<String, Boolean>> expectedResultList = new ArrayList<HashMap<String, Boolean>>();
+    private HashMap<String, Boolean> map1 = new HashMap<String, Boolean>();
+    private HashMap<String, Boolean> map2 = new HashMap<String, Boolean>();
+    private HashMap<String, Boolean> map3 = new HashMap<String, Boolean>();
+
     @Test
-     public void testProximity() throws Exception {
+    public void testProximity() throws Exception {
         logger.info("TestProximity");
 
         data.clear();
-        expectedResultId = new ArrayList<String>();
         eventCount = 0;
-
         data.add(new Object[]{"1", 0d, 0d});
         data.add(new Object[]{"2", 1d, 1d});
         data.add(new Object[]{"3", 2d, 2d});
         data.add(new Object[]{"1", 1.5d, 1.5d});
-        expectedResultId.add("2");
-        expectedResult.add(true);
-        expectedResultId.add("3");
-        expectedResult.add(true);
+        map1.put("2", true);
+        map1.put("3", true);
+        expectedResultList.add(map1);
+
         data.add(new Object[]{"1", 1.6d, 1.6d});
         data.add(new Object[]{"2", 5d, 5d});
-        expectedResultId.add("1");
-        expectedResult.add(false);
+        map2.put("1", false);
+        expectedResultList.add(map2);
+
         data.add(new Object[]{"1", 2d, 2d});
         data.add(new Object[]{"1", 5.5d, 5.5d});
-        expectedResultId.add("2");
-        expectedResult.add(true);
-        expectedResultId.add("3");
-        expectedResult.add(false);
+        map3.put("2", true);
+        map3.put("3", false);
+        expectedResultList.add(map3);
 
         String executionPlan = "@config(async = 'true')" +
                 "define stream dataIn (id string, longitude double, latitude double);"
@@ -77,16 +80,16 @@ public class GeoProximityTestCase extends GeoTestCase {
                 EventPrinter.print(timeStamp, inEvents, removeEvents);
                 for (Event event : inEvents) {
                     Boolean proximity = (Boolean) event.getData(0);
-                    Assert.assertEquals(expectedResult.get(eventCount), proximity);
                     String other = (String) event.getData(1);
-                    Assert.assertEquals(expectedResultId.get(eventCount), other);
-                    eventCount++;
+                    Assert.assertTrue(expectedResultList.get(eventCount).containsKey(other));
+                    Assert.assertEquals(expectedResultList.get(eventCount).get(other), proximity);
                 }
+                eventCount++;
             }
         });
         executionPlanRuntime.start();
         generateEvents(executionPlanRuntime);
         Thread.sleep(1000);
-        Assert.assertEquals(expectedResult.size(), eventCount);
+        Assert.assertEquals(expectedResultList.size(), eventCount);
     }
 }
