5c22cbad8e9d6039182f22fd13de1a30b2dbc0be
AnuGayan <anushaj@wso2.com>
2017-08-29 19:36:45 +0550

    Deployed 04a133d with MkDocs version: 0.16.3

---
+++ javascripts/extra.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2017 WSO2 Inc. (http://wso2.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var logoTitle = document.querySelector('.md-logo').title;
+var extentionTitle = logoTitle.slice(7);
+var header = document.querySelector('.md-header-nav__title');
+
+header.innerHTML = '<span class="extention-title">' + extentionTitle + '</span>' + header.textContent;
+    
